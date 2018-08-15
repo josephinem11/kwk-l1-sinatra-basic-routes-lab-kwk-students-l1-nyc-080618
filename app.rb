614e41e36948b9d71e51f5fcfc669360bfc5385d
josephinem11 <josephine.man0112@yahoo.com>
2018-08-15 13:35:46 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,8 +7,8 @@ class App < Sinatra::Base
   get '/hometown' do
     "My hometown is Staten Island"
   end
-  get '/favorite-song' do
-    "My favorite-song is Havana"
+  get '/favorite_song' do
+    "My favorite_song is Havana"
   end
 
 end
